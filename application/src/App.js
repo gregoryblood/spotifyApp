1aba7a93e28bac2ff9310040b9085026ba422904
Gregory Blood <gregoryblood1998@gmail.com>
2020-02-25 12:45:15 -0800

    Redirect

--- application/src/App.js
+++ application/src/App.js
@@ -12,22 +12,34 @@ const globalStyles = css`
     font-weight: 300;
   }
 `;
+
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
+var params = getHashParams();
+var access_token = params.access_token,
+            refresh_token = params.refresh_token,
+            error = params.error;
+
+
 const headers = {
-  Authorization: `Bearer ${getAccessToken()}`,
+  Authorization: `Bearer ${access_token}`,
   'Content-Type': 'application/json',
 };
-function getAccessToken () {
-  axios.post(`https://accounts.spotify.com/api/token`)
-  .then(res => {
-    console.log(res);
-    console.log(res.data);
-  })
-}
-const getUser = () => axios.get('https://api.spotify.com/v1/me', { headers });
+
+
+
 function App() {
   return (
     <div>
       <LoginScreen />
+      <h2>{access_token}</h2>
     </div>
   );
 }
